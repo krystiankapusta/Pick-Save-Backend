e7c825972dd789a67c1bbe54861f03241adea80d
krystiankapusta <krystian.kapusta@student.up.krakow.pl>
2025-05-23 22:36:15 +0200

    Implement signup, login and email confirmation

---
+++ Auth_Service/src/main/java/Pick_Save/Auth_Service/Config/AsynConfiguration.java
@@ -0,0 +1,9 @@
+package Pick_Save.Auth_Service.Config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@Configuration
+@EnableAsync
+public class AsynConfiguration {
+}
