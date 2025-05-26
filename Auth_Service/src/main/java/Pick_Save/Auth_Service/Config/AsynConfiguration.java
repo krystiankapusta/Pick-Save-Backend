b0ad9ebc63634a4ca84879697efb89e5e312235d
krystiankapusta <krystian.kapusta@student.up.krakow.pl>
2025-05-26 11:14:09 +0200

    Remove old backend files from previous location

--- Auth_Service/src/main/java/Pick_Save/Auth_Service/Config/AsynConfiguration.java
+++
@@ -1,9 +0,0 @@
-package Pick_Save.Auth_Service.Config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.scheduling.annotation.EnableAsync;
-
-@Configuration
-@EnableAsync
-public class AsynConfiguration {
-}
