64b4c3612fefc4cbb54d0ff5e46c90e7cbf00dd7
krystiankapusta <krystian.kapusta@student.up.krakow.pl>
2025-07-02 17:38:48 +0200

    Add Product-Service and update environment variable names

---
+++ Product_Service/src/test/java/Pick_Save/Product_Service/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package Pick_Save.Product_Service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
